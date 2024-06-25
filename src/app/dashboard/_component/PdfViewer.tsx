0d21c4495179b84a3d747df7fa33de5e761a14ca
Prabhakar <wassupprabhakar@gmail.com>
2024-06-25 12:50:47 +0530

    pdf viewer added

---
+++ src/app/dashboard/_component/PdfViewer.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+const PdfViewer = () => {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+  ).toString();
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+  const pdfUrl = process.env.NEXT_PUBLIC_PDF_URL;
+  return (
+    <>
+      <div>
+        <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default PdfViewer;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,6 +16,7 @@ import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { ReadContract } from "./_wagmi";
 import Balance from "./_component/Balance";
+import PdfViewer from "./_component/PdfViewer";
 
 
 export default function Home() {
@@ -38,7 +39,7 @@ export default function Home() {
       if (!authenticated) {
         redirect("/");
       } else {
-        console.log("user -> ", user?.linkedAccounts?.[0]);
+        //console.log("user -> ", user?.linkedAccounts?.[0]);
         setUserName((user?.linkedAccounts?.[0] as { name: string })?.name || "");
       }
     }
@@ -50,52 +51,53 @@ export default function Home() {
 
     walletClient?.getChainId().then((chainId) => {
       //console.log("chain id = ", wallets);
-      if(chainId !== 8453) {
-        console.log("not set chain");
-        switchChain({chainId: 8453});
+      if (chainId !== 8453) {
+        //console.log("not set chain");
+        switchChain({ chainId: 8453 });
       }
       setChainId(chainId.toString());
     });
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [walletClient]);
 
   return (
-    <div className="p-4 lg:p-8 flex justify-center items-center h-screen ">
-      <Card className="overflow-x-auto">
-        <CardHeader>
-          <CardTitle>Welcome {userName}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div>
-            <p>User Id :  <b>{user?.id}</b></p>
-            <ul>
+    <>
+      <div className="p-4 lg:p-8 flex justify-center items-center h-screen ">
+        <Card className="overflow-x-auto">
+          <CardHeader>
+            <CardTitle>Welcome {userName}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p>User Id :  <b>{user?.id}</b></p>
+              <ul>
 
-              <li>Wallet: {user?.wallet ? user?.wallet.address : 'None'}</li>
-            </ul>
-          </div>
-          <div className="pt-3">
-            {user &&
-              <div>
-                <Button variant="secondary" onClick={() => setShowContract(!showContract)}>Read Contract</Button>
-              </div>
-            }
-
-            {showContract && user?.wallet?.address &&
-              <>
-                <div className="pt-3">
-                  <ReadContract />
+                <li>Wallet: {user?.wallet ? user?.wallet.address : 'None'}</li>
+              </ul>
+            </div>
+            <div className="pt-3">
+              {user &&
+                <div>
+                  <Button variant="secondary" onClick={() => setShowContract(!showContract)}>Read Contract</Button>
                 </div>
-              </>
-            }
-          </div>
-          <div>
-            {chainId &&
-              <div>
-                Chain Id : {chainId}
-              </div>
-            }
-            {/* {user &&
+              }
+
+              {showContract && user?.wallet?.address &&
+                <>
+                  <div className="pt-3">
+                    <ReadContract />
+                  </div>
+                </>
+              }
+            </div>
+            <div>
+              {chainId &&
+                <div>
+                  Chain Id : {chainId}
+                </div>
+              }
+              {/* {user &&
               <textarea
                 value={JSON.stringify(user, null, 2)}
                 className="mt-2 w-full rounded-md bg-slate-700 p-4 font-mono text-xs text-slate-50 sm:text-sm"
@@ -103,12 +105,21 @@ export default function Home() {
                 disabled
               />
             } */}
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline" onClick={logout}>Logout</Button>
-        </CardFooter>
-      </Card>
-    </div>
+            </div>
+
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline" onClick={logout}>Logout</Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="pt-8">
+        <Card>
+          <CardContent>
+            <PdfViewer />
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
