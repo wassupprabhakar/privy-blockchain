555a831e9001f2e1550ab66cb583955ba61fc315
Prabhakar <wassupprabhakar@gmail.com>
2024-06-25 12:58:44 +0530

    pdf issue ressolved

--- src/app/dashboard/_component/PdfViewer.tsx
+++ src/app/dashboard/_component/PdfViewer.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 const PdfViewer = () => {
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
-  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
+  
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
   }
